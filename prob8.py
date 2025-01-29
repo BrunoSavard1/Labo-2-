156912d5677996473b2a88d1a330bbe7d1aff0cd
BrunoSavard1 <brunosavard112@gmail.com>
2025-01-29 15:08:58 -0500

    Update prob8.py

--- prob8.py
+++ prob8.py
@@ -1,8 +1,9 @@
 def addition(a, b):
+    # après le copier coller faire attention au espace de la fonction locale
     resultat = a + b
     return resultat
+# le 5 était écrit en str à cause des guillemets 
 x = 5
 y = 3
 z = addition(x, y)
 print("Résultat:", z)
-
