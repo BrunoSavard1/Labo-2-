b29c7b7921073170e2069d607cb8ba4988ce66bf
BrunoSavard1 <brunosavard112@gmail.com>
2025-01-29 14:49:50 -0500

    Update prob6.py

--- prob6.py
+++ prob6.py
@@ -1,3 +1,4 @@
+# faut mettre float pour que le programme considère que c'est un chiffre au lieu d'une str
 nombre = float(input("Entrez un nombre : "))
 resultat = nombre * 2
 print("Le double de votre nombre est :", resultat)
