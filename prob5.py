f441ee1e011d19004eb2bc36c5f107a159610808
BrunoSavard1 <brunosavard112@gmail.com>
2025-01-29 14:37:37 -0500

    Update prob5.py

---
+++ prob5.py
@@ -0,0 +1,4 @@
+"""poser la question ( avez-vous fini?) si oui fin du programme
+sinon poser la 2Q (fait il plus chaud que 20 degrée celsius)
+si oui retour a la question 1 
+sinon allumer le chauffage et finir le programme"""
